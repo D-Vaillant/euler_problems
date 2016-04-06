69eb3737f50600e713648c0307a4b4715df65bdc
D-Vaillant <dvaillant@gmail.com>
2016-04-06 13:19:30 -0700

    Finished Problem 14. Memoization, ho.

--- problem014.py
+++ problem014.py
@@ -19,12 +19,49 @@
 
 class CollatzIterator():
     """Takes an integer, runs the Collatz iteration until termination."""
+    # Memoization dict.
+    seen_before = {}
+
     def __init__(self, a):
         """Verifies the integer."""
         if not isinstance(a, int): raise TypeError("The non-integer case is trivial or nonsense.") 
+        self.original = a
+        self.a = a
+        self.steps = 0
 
-    def __iter__(self):
-        if n%2:
-    def __bool__(self, a):
-        return a == 1
-        
+    def __next__(self):
+        old = self.a
+        self.steps += 1
+        if self.a & 1:
+            self.a = 3*self.a + 1
+        else:
+            self.a //= 2
+        # print("{} => {}".format(old, self.a))
+
+    def __bool__(self):
+        return self.a == 1
+
+    def run(self):
+        while not (self.a in CollatzIterator.seen_before or bool(self)):
+            next(self)
+
+        try:
+            total_steps = self.steps + CollatzIterator.seen_before[self.a]
+            CollatzIterator.seen_before[self.original] = total_steps
+            return total_steps
+        except KeyError:
+            CollatzIterator.seen_before[self.original] = self.steps
+            return self.steps
+
+if __name__ == "__main__":
+    candidate = 0
+    cand_value = 3
+    for x in range(3, 1000000):
+        a = CollatzIterator(x)
+        series = a.run()
+        if series > candidate:
+            candidate = series
+            cand_value = x
+        else: pass
+
+    print(cand_value)
