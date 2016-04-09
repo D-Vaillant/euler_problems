363dc95106cc0bf8d867dbf0818a079963da940d
D-Vaillant <dvaillant@gmail.com>
2016-04-08 20:46:27 -0700

    Some refactoring for brevity and wit.

--- problem015.py
+++ problem015.py
@@ -1,44 +1,21 @@
 """ problem015.py """
 # This is just a combinatorics problem. 
 
-class GridShape():
-    def __init__(self, down = None, right = None):
-        self.d = down
-        self.r = right
-
-        self.paths = 0
-        try: self.paths += self.d.paths
-        except AttributeError: pass
-
-        try: self.paths += self.r.paths
-        except AttributeError: pass
-
-        if self.paths == 0 : raise Exception("Call SquareShape instead.")
-
-class SinglePath(GridShape):
-    def __init__(self, **kwargs):
-        self.paths = 1
-
-class SquareShape(GridShape):
-    def __init__(self, **kwargs):
-        self.d = None
-        self.r = None
-        self.paths = 2
-
 class GridManager():
     """ The key is MEMOIZATION! """
     """ Because before I was getting recursion errors. """
     def __init__(self):
         self.grid_dict = {}
-        self.grid_dict[(1, 1)] = SquareShape()
         for _ in range(0, 22):
-            self.grid_dict[(_, 0)] = SinglePath()
-            self.grid_dict[(0, _)] = SinglePath()
+            self.grid_dict[(_, 0)] = 1
+            self.grid_dict[(0, _)] = 1
+
+    def get(self, n, m):
+        if (n, m) not in self.grid_dict:
+            self.grid_dict[(n, m)] = self.run(n, m)
+        return self.grid_dict[(n, m)]
 
     def run(self, n, m):
-        try: return self.grid_dict[(n,m)]
-        except KeyError: pass
-
         if (n-1, m) not in self.grid_dict:
             self.grid_dict[(n-1, m)] = self.run(n-1, m)
         if (n, m-1) not in self.grid_dict:
@@ -46,10 +23,13 @@ class GridManager():
         return self.process(n, m)
 
     def process(self, n, m):
-        return GridShape(down = self.grid_dict[(n, m-1)],
-                         right = self.grid_dict[(n-1, m)])
+        return self.grid_dict[(n, m-1)] + self.grid_dict[(n-1, m)]
 
 
 if __name__ == "__main__":
     G = GridManager()
-    print(G.run(20, 20).paths)
+    tst = lambda x: print(G.get(x, x))
+    tst(2)
+    tst(3)
+    tst(4)
+    tst(20)
