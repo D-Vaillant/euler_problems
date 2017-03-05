ea5bec0a692d0bcbec0c52fc7f2a8c609fb2a0af
D-Vaillant <dvaillant@gmail.com>
2017-03-05 14:17:41 -0800

    Optimized problem010 with a prime sieve.

--- problem001.py
+++ problem001.py
@@ -1,15 +1,13 @@
 """ problem001.py:
         Find the sum of all the multiples of 3 or 5 below 1000.
 """
-from __future__ import division  # lol python2
+from __future__ import division  # python2
 
 
 def arith(lower, upper):
-    while upper%lower:
-        upper -= 1
+    upper -= upper%lower
     # print("({} + {})/2 * {}".format(lower, upper, upper/lower))
     return (lower + upper)/2 * (upper/lower)
-
 
 def main(max_):
     return arith(3, max_) + arith(5, max_) - arith(15, max_)
--- problem010.py
+++ problem010.py
@@ -1,22 +1,20 @@
 """ problem010.py:
         Finds the sum of the primes below two million. """
 
-def main():
-    """Implements a sieve to find the primes, sums them while doing so."""
-    primes = [p for p in range(2000000) if isPrime(p)]
-    return sum(primes)
+from functools import lru_cache
+from math import sqrt, ceil
 
-def isPrime(n):
-    """Implementation of a naive primality test."""
-    if n <= 1: return False
-    elif n <= 3: return True
-    elif n%2 == 0 or n%3 == 0: return False
 
-    i = 5
-    while i*i <= n:
-        if n%i == 0 or n%(i+2) == 0: return False
-        i += 6
-    return True
+def prime_sieve(limit):
+    a = [True] * limit
+    a[0] = a[1] = False
+
+    for (i, is_prime) in enumerate(a):
+        if is_prime:
+            yield i
+            for n in range(i*i, limit, i):
+                a[n] = False
+
 
 if __name__ == "__main__":
-    print(main())
+    print(sum(prime_sieve(2000000)))
